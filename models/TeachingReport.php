09efe5114314dc016680d9aaa20d8878ec91de6d
RPZXD <rpz.tiwa@phichai.ac.th>
2025-05-04 01:12:31 +0700

    Update TeachingReport.php

--- models/TeachingReport.php
+++ models/TeachingReport.php
@@ -9,15 +9,9 @@ class TeachingReport
 
     public function __construct()
     {
-        $host = 'localhost';
-        $dbname = 'phichaia_cktech';
-        $username = 'root';
-        $password = '';
-        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-        $this->pdo = new PDO($dsn, $username, $password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ]);
+        require_once __DIR__ . '/../classes/DatabaseTeachingReport.php';
+        $db = new \App\DatabaseTeachingReport();
+        $this->pdo = $db->getPDO();
     }
 
     public function getAllByTeacher($teacher_id)
